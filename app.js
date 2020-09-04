afb4d69c5200f60823e106666585248db87440f7
aayush-sinha <aayush_sinha@icloud.com>
2020-09-04 23:33:18 +0530

    test

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
@@ -26,7 +25,6 @@ mongoose
 //ROUTES
 
 //Auth
-
 const {check, validationResult} = require("express-validator");
 const {signout,signup,signin, isSignedIn} = require("./controllers/auth");
 
@@ -60,11 +58,50 @@ app.get("/categories", getAllCategory);
 app.put("/category/:categoryId/:userId", isSignedIn, isAuthenticated, isAdmin, updateCategory);
 app.delete("/category/:categoryId/:userId", isSignedIn, isAuthenticated, isAdmin, removeCategory);
 
+//order
+const { pushOrderInPurchaseList } = require("./controllers/user");
+const { updateStock } = require("./controllers/product");
+const { getOrderById, createOrder, getAllorders, updateStatus, getOrderStatus } = require("./controllers/order");
+app.param("userId", getUserByID);
+app.param("orderId", getOrderById);
+app.post("order/create/:userId", isSignedIn, isAuthenticated, pushOrderInPurchaseList, updateStock, createOrder);
+app.get("/order/all/:userId", isSignedIn, isAuthenticated, isAdmin, getAllorders);
+app.get("/order/status/:userId", isSignedIn, isAuthenticated, isAdmin, getOrderStatus);
+app.put("/order/:orderId/status/:userId", isSignedIn, isAuthenticated, isAdmin, updateStatus);
+
+//payment
+const { getToken, processPayment } = require("./controllers/paymentb");
+app.param("userId", getUserByID);
+app.get("/payment/gettoken/:userId", isSignedIn, isAuthenticated, getToken);
+app.post(
+  "/payment/braintree/:userId",
+  isSignedIn,
+  isAuthenticated,
+  processPayment
+);
+
+//product
+const { getProductById, createProduct, getProduct, photo, removeProduct, updateProduct, getAllProducts, getAllUniqueCategories } = require("./controllers/product");
+app.param("userId", getUserByID);
+app.param("productId", getProductById);
+app.post("/product/create/:userId", isSignedIn, isAuthenticated, isAdmin, createProduct);
+app.get("/product/:productId", getProduct);
+app.get("/product/photo/:productId", photo);
+app.delete("/product/:userId/:productId", isSignedIn, isAuthenticated, isAdmin, removeProduct);
+app.put("/product/:userId/:productId",  isSignedIn, isAuthenticated, isAdmin, updateProduct);
+app.get("/products", getAllProducts);
+app.get("/products/categories", getAllUniqueCategories);
 
 
+//user
 
+const { getUser, updateUser, userPurchaseList, successMessage } = require("./controllers/user");
+app.param("userId", getUserByID);
+app.get("/user/:userId", isSignedIn, isAuthenticated ,getUser);
+app.put("/user/:userId", isSignedIn, isAuthenticated, updateUser);
+app.get("user/orders/:userId", isSignedIn, isAuthenticated, userPurchaseList);
+app.get("user/orders/success", successMessage);
 
- 
 //PORTS
 const port = process.env.PORT || 8000;
 
