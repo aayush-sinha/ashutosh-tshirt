c31f4d21cd91227732d6eb1a03f20dc3c6ac89df
aayush-sinha <aayush_sinha@icloud.com>
2020-09-04 22:06:13 +0530

    test

--- app.js
+++ app.js
@@ -8,23 +8,23 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 //ROUTES IMPORT
-// const authRoutes = require("./routes/auth");
-// const userRoutes = require("./routes/user");
-// const categoryRoutes = require("./routes/category");
-// const productRoutes = require("./routes/product");
-// const orderRoutes = require("./routes/order");
-// const paymentBRoutes = require("./routes/paymentbRoutes");
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+const orderRoutes = require("./routes/order");
+const paymentBRoutes = require("./routes/paymentbRoutes");
 
 //Middlewares
-// app.use(bodyParser.json());
-// app.use(cookieParser());
-// app.use(cors());
-// app.use("/api", authRoutes);
-// app.use("/api", userRoutes);
-// app.use("/api", categoryRoutes);
-// app.use("/api", productRoutes);
-// app.use("/api", orderRoutes);
-// app.use("/api", paymentBRoutes);
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+app.use("/api", orderRoutes);
+app.use("/api", paymentBRoutes);
 
 //Testing
 // app.get("/success",(req, res)=>{
