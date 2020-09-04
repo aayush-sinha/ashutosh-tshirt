91ac7c65712dd5a8d08956185591d50f4dcaa155
aayush-sinha <aayush_sinha@icloud.com>
2020-09-04 22:57:40 +0530

    test

--- app.js
+++ app.js
@@ -1,45 +1,20 @@
 require("dotenv").config();
-const {signout,signup,signin, isSignedIn} = require("./controllers/auth");
+
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const User = require("./models/user");
-const { check, validationResult } = require("express-validator");
-var jwt = require("jsonwebtoken");
-var expressJwt = require("express-jwt");
-//ROUTES IMPORT
-const authRoutes = require("./routes/auth");
-const userRoutes = require("./routes/user");
-const categoryRoutes = require("./routes/category");
-const productRoutes = require("./routes/product");
-const orderRoutes = require("./routes/order");
-const paymentBRoutes = require("./routes/paymentbRoutes");
 
 //Middlewares
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
-app.use(authRoutes);
-app.use(userRoutes);
-app.use(categoryRoutes);
-app.use(productRoutes);
-app.use(orderRoutes);
-app.use(paymentBRoutes);
-
-//Testing
-// app.get("/success",(req, res)=>{
-//   res.json({
-//     error: "no error"
-//   })
-// })
-
 
 //DB CONNECTION
 mongoose
-  .connect('mongodb+srv://ashutosh07:ashutosh07@wearyourteecluster.igpz5.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -48,13 +23,45 @@ mongoose
     console.log("DB CONNECTED");
   });
   
-
-
 //ROUTES
-app.get('/',(req,res)=>{
-  res.send(200)
+
+//Auth
+
+const {check, validationResult} = require("express-validator");
+const {signout,signup,signin, isSignedIn} = require("./controllers/auth");
+
+app.post("/signup",[
+    check("name", "name should be atleast 3 letters").isLength({min: 3}),
+    check("email", "email required").isEmail(),
+    check("password").isLength({min: 8}).withMessage("Password should be atleast of 8 letters")
+],signup);
+
+app.post("/signin",[
+    check("email", "email required").isEmail(),
+    check("password").isLength({min: 1}).withMessage("Password field is req")
+],signin);
+
+app.get("/signout",signout);
+
+app.get("/testroute", isSignedIn, (req,res)=>{
+    res.json(req.auth);
 })
-app.post("/signup",signup);
+
+// category
+
+const { getCategoryByID, createCategory, getCategory, getAllCategory, updateCategory, removeCategory } = require("./controllers/category");
+const { isAuthenticated, isAdmin } = require("./controllers/auth");
+const { getUserByID } = require("./controllers/user");
+app.param("userId", getUserByID);
+app.param("categoryId", getCategoryByID);
+app.post("/category/create/:userId", isSignedIn, isAuthenticated, isAdmin, createCategory);
+app.get("/category/:categoryId", getCategory);
+app.get("/categories", getAllCategory);
+app.put("/category/:categoryId/:userId", isSignedIn, isAuthenticated, isAdmin, updateCategory);
+app.delete("/category/:categoryId/:userId", isSignedIn, isAuthenticated, isAdmin, removeCategory);
+
+
+
 
 
  
